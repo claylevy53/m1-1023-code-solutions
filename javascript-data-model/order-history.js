78450cf7c874e707e7a734dd05b1b3ed79d40339
Clay Levy <claylevy53@gmail.com>
2023-10-27 03:30:28 +0000

    completed javascript-data-model - clay levy

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-octal */
+const orderHistory = [
+  {
+    itemOrdered: 'Javascript for impatient programmers',
+    deliveredDate: '2020-08-8',
+    shippedTo: 'JS Masher',
+    orderPlaced: '2020-08-04',
+    additionalInfo: [{ price: 31.55, returnWindow: '2020-09-07' }],
+  },
+  {
+    itemOrdered: 'The Timeless Way of Building',
+    deliveredDate: '2020-07-20',
+    shippedTo: 'JS Masher',
+    orderPlaced: '2020-07-19',
+    additionalInfo: [{ price: 31.55, returnWindow: '2020-08-19' }],
+  },
+  {
+    itemOrdered: 'Gamecube controller adapter',
+    deliveredDate: '2020-07-07',
+    shippedTo: 'JS Masher',
+    orderPlaced: '2020-07-03',
+    additionalInfo: [{ price: 15.98, returnWindow: '2020-08-05' }],
+  },
+  [
+    {
+      itemOrdered: 'Gamecube controller',
+      deliveredDate: '2020-07-03',
+      shippedTo: 'JS Masher',
+      orderPlaced: '2020-07-03',
+      additionalInfo: [{ price: 94.95, returnWindow: '2020-08-04' }],
+    },
+    {
+      itemOrdered: 'The Art of Sql',
+      deliveredDate: '2020-07-03',
+      shippedTo: 'JS Masher',
+      orderPlaced: '2020-07-03',
+      additionalInfo: [{ price: 33.99, returnWindow: '2020-08-04' }],
+    },
+  ],
+];
