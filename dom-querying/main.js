c4d34f46b8c68821841282218d2f58a21e95a619
Clay Levy <claylevy53@gmail.com>
2023-12-17 19:46:31 +0000

    Completed dom-querying exercise

---
+++ dom-querying/main.js
@@ -0,0 +1,18 @@
+const h1 = document.querySelector('h1');
+
+console.log(h1);
+console.dir(h1);
+
+const idExplanation = document.querySelector('#explanation');
+console.log(idExplanation);
+console.dir(idExplanation);
+
+const classAttribute = document.querySelector('.hint');
+console.log(classAttribute);
+console.dir(classAttribute);
+
+const nodeList = document.querySelectorAll('p');
+console.log(nodeList);
+
+const exampleLinkClass = document.querySelectorAll('.example-link');
+console.log(exampleLinkClass);
