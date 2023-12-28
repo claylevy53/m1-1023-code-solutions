fcadf6a01c2e66b3e56965a6d5f12abe07d658f3
Clay Levy <claylevy53@gmail.com>
2023-12-28 02:36:26 +0000

    Completed dom-event-delegation exercise

---
+++ dom-event-delegation/main.js
@@ -0,0 +1,14 @@
+const $uL = document.querySelectorAll('ul');
+
+$uL.forEach(function (uL) {
+  uL.addEventListener('click', function (event) {
+    if (event.target.tagName === 'BUTTON') {
+      const removeItem = event.target.closest('.task-list-item');
+      console.log('Closest Parent:', removeItem);
+      removeItem.remove();
+    } else {
+      console.log('Event.target:', event.target);
+      console.log('Event.target.TagName:', event.target.tagName);
+    }
+  });
+});
