4201a55687c0af51fae06ff532c85b404c49a112
Clay Levy <claylevy53@gmail.com>
2024-01-02 02:40:11 +0000

    Completed dom-event-delegation exercise with fixes

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -1,14 +1,12 @@
-const $uL = document.querySelectorAll('ul');
+const $uL = document.querySelector('ul');
 
-$uL.forEach(function (uL) {
-  uL.addEventListener('click', function (event) {
-    if (event.target.tagName === 'BUTTON') {
-      const removeItem = event.target.closest('.task-list-item');
-      console.log('Closest Parent:', removeItem);
-      removeItem.remove();
-    } else {
-      console.log('Event.target:', event.target);
-      console.log('Event.target.TagName:', event.target.tagName);
-    }
-  });
+$uL.addEventListener('click', function (event) {
+  if (event.target.tagName === 'BUTTON') {
+    const removeItem = event.target.closest('.task-list-item');
+    console.log('Closest Parent:', removeItem);
+    removeItem.remove();
+  } else {
+    console.log('Event.target:', event.target);
+    console.log('Event.target.TagName:', event.target.tagName);
+  }
 });
